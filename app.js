97c2b6e2796df0a26cb0c5622806e900084d3632
Adilet <130757762+AdiletAbdulazizov@users.noreply.github.com>
2023-05-03 00:18:02 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('[data-counter]');
+
+if (counters) {
+	counters.forEach(counter => {
+		counter.addEventListener('click', e => {
+			const target = e.target;
+
+			if (target.closest('.counter__button')) { 
+				let value = parseInt(target.closest('.counter').querySelector('input').value);
+
+				if (target.classList.contains('counter__button_plus')) {
+					value++;
+				} else {
+					--value;
+				}
+
+				target.closest('.counter').querySelector('input').value = value;
+			}
+		})
+	})	
+}
